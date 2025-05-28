2d6ea2c8058b5c38a66d68182c2ab164ab1d474d
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-05-28 23:35:12 +0530

    refactor: restructure server.js to initialize database and update server start logic

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,34 @@
 import express from 'express';
 import { configDotenv } from 'dotenv';
-import { sql } from './config/db';
+import {sql} from './config/db.js'
 const app=express();
-
 configDotenv();
 
 app.get('/', (req,res)=>{
     res.send("Hello Anirban Here it is working fine")
 
 })
-
-app.listen(process.env.PORT || 5001, ()=>{
-    console.log(`Server is running at port ${process.env.PORT}`)
-})
+async function initDB(){
+    try {
+        await sql `CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10, 2) NOT NULL,
+        category VARCHAR(50) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_DATE
+        )`
+        console.log("Database initialised succesfully")
+    }
+    catch (error) {
+        console.error("Error initializing database:", error);process.exit(1)
+    }
+}
+// app.listen(process.env.PORT || 5001, ()=>{
+//     console.log(`Server is running at port ${process.env.PORT}`)
+// })
+ initDB().then (()=>{
+    app.listen(process.env.PORT || 5001, ()=>{
+        console.log(`Server is running at port ${process.env.PORT || 5001}`)
+    }  )
+ })
