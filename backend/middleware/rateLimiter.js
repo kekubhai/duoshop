5d287d894f21eb4b160628c79896262ac12f38c0
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-05-29 13:58:23 +0530

    refactor: add rate limiting functionality using Upstash Redis

---
+++ backend/middleware/rateLimiter.js
@@ -0,0 +1,14 @@
+import rateLimiter from "../config/upstash";
+const rateLimiter=async(req,res)=>{
+    try {
+    const {success}=await rateLimiter.limit("my-rate-limit")
+    if (!success) {
+        return res.status(429).json({ error: "Rate limit exceeded" });
+    }
+    // Continue with the request
+    } catch(error){
+        console.error("Rate limiter error:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+export default rateLimiter;
