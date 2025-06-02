ea4ae1442661fc2be561a13f69faa47b1a7d841d
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-06-02 17:50:22 +0530

    feat: Add SafeScreen component and formatDate utility; update .gitignore and create styles

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,9 +1,23 @@
 import { Stack } from "expo-router";
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
+import { ClerkProvider } from '@clerk/clerk-expo'
+import SafeScreen from "@/components/SafeScreen";
+import { Slot } from 'expo-router'
 export default function RootLayout (){
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+
+
+    <ClerkProvider tokenCache={tokenCache}>
+      <Slot />
+    
+    <SafeScreen>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" options={{ headerShown: false }} />
+      </Stack>
+    </SafeScreen>
+    </ClerkProvider>
   );
 }
