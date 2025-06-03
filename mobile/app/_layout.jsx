4fc696b2c8f118e1269ad594303cc6350dd20bde
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-06-03 17:53:08 +0530

    clerk setup done

---
+++ mobile/app/_layout.jsx
@@ -0,0 +1,17 @@
+import { Stack } from "expo-router";
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { ClerkProvider } from '@clerk/clerk-expo'
+import SafeScreen from "@/components/SafeScreen";
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Stack initialRouteName="(root)">
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(root)" options={{ headerShown: false }} />
+        </Stack>
+      </SafeScreen>
+    </ClerkProvider>
+  );
+}
